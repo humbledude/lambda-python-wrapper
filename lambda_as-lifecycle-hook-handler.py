2a576c645ad49121188cdcd481a375f2e45afff2
humbledude <keunhui.park@gmail.com>
2017-05-04 17:25:39 +0900

    complete lifecycle action

--- lambda_as-lifecycle-hook-handler.py
+++ lambda_as-lifecycle-hook-handler.py
@@ -2,30 +2,31 @@ import json
 import boto3
 import time
 
-print('as-lifecycle-hook-handler')
-
-client = boto3.client('autoscaling')
-
-
 def do_something():
     print('do something..')
     time.sleep(2)
     print('done')
 
+def as_complete_lifecycle_action(lifecycleHookName, autoScalingGroupName, instanceId):
+    print('as_complete_lifecycle_action : {}, {}, {}'.format(lifecycleHookName, autoScalingGroupName, instanceId))
+    as_client = boto3.client('autoscaling')
+    return as_client.complete_lifecycle_action(
+        LifecycleHookName=lifecycleHookName,
+        AutoScalingGroupName=autoScalingGroupName,
+        InstanceId=instanceId,
+        LifecycleActionResult='CONTINUE'
+    )
+
 
 def lambda_handler(event, context):
-    print('lambda-handler')
+    print('lambda_handler')
     print("Received event: " + json.dumps(event, indent=2))
     message = json.loads(event['Records'][0]['Sns']['Message'])
-    # metadata = message['NotificationMetatdata']
 
     do_something()
 
-    response = client.complete_lifecycle_action(
-        LifecycleHookName=message['LifecycleHookName'],
-        AutoScalingGroupName=message['AutoScalingGroupName'],
-        LifecycleActionResult='CONTINUE',
-        InstanceId=message['EC2InstanceId']
-    )
-    print(response)
+    response = as_complete_lifecycle_action(message['LifecycleHookName'],
+                                            message['AutoScalingGroupName'],
+                                            message['EC2InstanceId'])
 
+    print(response)
